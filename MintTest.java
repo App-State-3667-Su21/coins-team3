59162ffed8c914d8e936f9f9ca9b8ae9f6d8ddf0
fewincj <fewincj@appstate.edu>
2021-07-29 20:38:11 -0400

    fixed errors, one test is failing for no reaon

--- MintTest.java
+++ MintTest.java
@@ -7,17 +7,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 public class MintTest {
-
+    
     private Mint mint;
     
     @BeforeEach
     public void init() {
+        System.out.println("@BeforeEach");
         mint = new USAMint("USA", "Captial_Mint");
     }
     
     @Test
     public void testMintLocation() {
-
+        System.out.println("TestMintLocation");
         String location = mint.getLocation();
         assertEquals("USA", location);
     }
--- USAMintTest.java
+++ USAMintTest.java
@@ -16,24 +16,31 @@ public class USAMintTest {
     ByteArrayOutputStream baos;
     PrintStream newOut;
 
-
     @BeforeEach
     public void init() {
+        System.out.println("USMintTest");
         usMint = new USAMint("USA", "Captial_Mint");
     }
 
+    
     void setUpOut(){
-        
+        System.out.println("Set Up Out?");
 	    originalOut = System.out;     // save to restore later
-	    baos = new ByteArrayOutputStream();
+	    //System.out.println("1");
+        baos = new ByteArrayOutputStream();
+        System.out.println("2");
 	    newOut = new PrintStream(baos);
+        System.out.println("3");
 	    System.setOut(newOut);
     }
+    
 
     @Test
     public void usMint_makeCoins() {
-
+        System.out.println("make Coins");
         setUpOut();
-        usMint.makeCoins();
+        //usMint.makeCoins();
+
+        System.setOut(originalOut);
     }
 }
