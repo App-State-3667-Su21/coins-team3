5d8b69a173a26da48c92b463b8e0d357da62b9d5
elliottim <87030880+elliottim@users.noreply.github.com>
2021-07-31 12:09:13 -0400

    Update Dollar.java

--- Dollar.java
+++ Dollar.java
@@ -1,6 +1,11 @@
 public class Dollar extends Coin {
     private String code = "USD";
     private double val = 1;
+    private String name = "Dollar";
+
+    public String getName() {
+        return this.name();   
+    }
 
     public String getCode() {
         return this.code;
