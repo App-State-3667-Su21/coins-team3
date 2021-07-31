9b2340a18b5c7bdc974af564e6ea72ae7636057a
elliottim <87030880+elliottim@users.noreply.github.com>
2021-07-31 12:10:06 -0400

    Update HalfDollar.java

--- HalfDollar.java
+++ HalfDollar.java
@@ -1,6 +1,11 @@
 public class HalfDollar extends Coin { 
     private String code = "USD";
     private double val = .5;
+    private String name = "HalfDollar";
+
+    public String getName() {
+        return this.name();   
+    }
 
     public String getCode() {
         return this.code;
