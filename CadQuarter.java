fe1fc11827756f247e1cb8fa5469e0d2218cf5e4
elliottim <87030880+elliottim@users.noreply.github.com>
2021-07-31 12:06:58 -0400

    Update CadQuarter.java

--- CadQuarter.java
+++ CadQuarter.java
@@ -1,7 +1,12 @@
 public class CadQuarter extends Coin {
     private String code = "CAD";
     private double val = 0.25;
+    private String name = "CadQuarter";
 
+    public String getName() {
+        return this.name();   
+    }
+    
     public String getCode() {
         return this.code;
     }
