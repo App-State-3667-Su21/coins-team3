ca12ac785e81ceb34120fae0a0daa8bd1b78144f
elliottim <87030880+elliottim@users.noreply.github.com>
2021-07-31 12:10:48 -0400

    Update Loonie.java

--- Loonie.java
+++ Loonie.java
@@ -1,6 +1,11 @@
 public class Loonie extends Coin {
     private String code = "CAD";
     private double val = 1;
+    private String name = "Toonie";
+
+    public String getName() {
+        return this.name();   
+    }
 
     public String getCode() {
         return this.code;
