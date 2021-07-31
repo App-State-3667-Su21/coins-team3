46fcfde071d53f2b678bde7041214e788ff89c9d
Ian Elliott <elliottim@appstate.edu>
2021-07-31 16:47:53 -0400

    Fixed Toonie

--- Toonie.java
+++ Toonie.java
@@ -1,11 +1,6 @@
 public class Toonie extends Coin {
     private String code = "CAD";
     private double val = 2;
-    private String name = "Toonie";
-
-    public String getName() {
-        return this.name();   
-    }
 
     public String getCode() {
         return this.code;
