003b334804ed835b8110ce5670d21a6813d23d3c
elliottim <87030880+elliottim@users.noreply.github.com>
2021-07-31 12:09:34 -0400

    Update FiftyCent.java

--- FiftyCent.java
+++ FiftyCent.java
@@ -1,6 +1,11 @@
 public class FiftyCent extends Coin { 
     private String code = "CAD";
     private double val = .5;
+    private String name = "FiftyCent";
+
+    public String getName() {
+        return this.name();   
+    }
 
     public String getCode() {
         return this.code;
