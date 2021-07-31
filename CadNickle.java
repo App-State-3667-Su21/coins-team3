4735a181826040ced87da433646db6c7dcd3f507
elliottim <87030880+elliottim@users.noreply.github.com>
2021-07-31 12:05:46 -0400

    Update CadNickle.java

--- CadNickle.java
+++ CadNickle.java
@@ -1,7 +1,13 @@
 public class CadNickle extends Coin {
     private String code = "CAD";
     private double val = 0.05;
+    private String name = "CadDime";
 
+    public String getName() {
+        return this.name();   
+    }
+    
+    
     public String getCode() {
         return this.code;
     }
