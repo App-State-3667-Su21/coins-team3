3a08e3a62c536b78afb1481dad4acb670bc9b833
elliottim <87030880+elliottim@users.noreply.github.com>
2021-07-31 12:13:42 -0400

    Update Quarter.java

--- Quarter.java
+++ Quarter.java
@@ -1,6 +1,11 @@
 public class Quarter extends Coin {
     private String code = "USD";
     private double val = 0.25;
+    private String name = "Quarter";
+
+    public String getName() {
+        return this.name();   
+    }
 
     public String getCode() {
         return this.code;
