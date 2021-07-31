61abda48df59c857201489ba9f0e64720b0e11f1
elliottim <87030880+elliottim@users.noreply.github.com>
2021-07-31 12:13:23 -0400

    Update Penny.java

--- Penny.java
+++ Penny.java
@@ -1,6 +1,11 @@
 public class Penny extends Coin{
     private String code = "USD";
     private double val = 0.01;
+    private String name = "Penny";
+
+    public String getName() {
+        return this.name();   
+    }
 
    
     public String getCode(){
